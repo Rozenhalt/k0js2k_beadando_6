f8f47eff171ea6c0dc15ff420330e0fc86eb5e9c
Rozenhalt <38649854+Rozenhalt@users.noreply.github.com>
2018-05-09 19:58:39 +0200

    Update SzotarMorse.py

--- SzotarMorse.py
+++ SzotarMorse.py
@@ -2,17 +2,6 @@ SzotarBetu={ 'a':'. -' , 'b':'- . . .' , 'c':'- . - .' , 'd':'- . .' , 'e':'.' ,
 
 SzotarMorse={ '. -':'a' , '- . . .':'b' , '- . - .':'c' , '- . .':'d' , '.':'e' , '. . - .':'f' , '- - .':'g' , '. . . .':'h' , '. .':'i' , '. - - -':'j' , '- . -':'k' , '. - . .':'l' , '- -':'m' , '- .':'n' , '- - -':'o' , '. - - .':'p' , '- - . -':'q' , '. - .':'r' , '. . .':'s' , '-':'t' , '. . -':'u' , '. . . -':'v' , '. - -':'w' , '- . . -':'x' , '- . - -':'y' , '- - . .':'z' , '. - - - -':'1' , '. . - - -':'2' , '. . . - -':'3' , '. . . . -':'4' , '. . . . .':'5' , '- . . . .':'6' , '- - . . .':'7' , '- - - . .':'8' , '- - - - .':'9' , '- - - - -':'0'}
 
-print('A Morse-kód szabályai:\n1. - A Morse-kódban szereplő betűk, szavak a .(pont) és a -(gondolatjel) karakterekből épülnek fel\n2. - Egy betűnek megfelelő jelek között a távolság 1 üres mező. Pl.: a: . -, k: - . -\n3. - Az azonos szóban szereplő betűk között a távolság 3 üres mező. Pl.: nagy: - .   . -   - - .   - . - -\n4. - Két szó között a távolság 7 üres mező kell hogy legyen.\n\n')
-print('A fordító kizárólag ékezetek nélküli szavakat és mondatokat fordít le,\nvalamit Morse-kódról való fordítást csak abban az esetben tud végezni, ha a Morse-kód helyesírási szabályai be vannak tartva.\n')
-print('Ha szeretné megtekinteni a Morse abc-t nyomjon egy "i" betűt, amennyiben tisztában van a jelölésekkel és tovább haladna nyomjon egy "n" betűt.')
-
-Eldontes=input()
-
-if Eldontes=='i':
-
-    for k,v in SzotarBetu.items():
-        print(k,':',v)
-
 print('Kérem a fordítani kívánt szót vagy mondatot:')
 BekertSzo=input()
 
